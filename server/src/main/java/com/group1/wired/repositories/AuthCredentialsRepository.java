bf639e9c86c7cf4cba5eb9a206647e1938a9c4a1
Martin <martin.mones@student.ateneo.edu>
2026-03-01 05:18:41 +0800

    filled AuthCredentialsRepository.java

--- server/src/main/java/com/group1/wired/repositories/AuthCredentialsRepository.java
+++ server/src/main/java/com/group1/wired/repositories/AuthCredentialsRepository.java
@@ -1,5 +1,15 @@
-package com.group1.wired.repositories;
+package com.group1.wired.repository;
 
-public class AuthCredentialsRepository {
+import com.group1.wired.entities.AuthCredentials;
+import com.group1.wired.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
+public interface AuthCredentialsRepository extends JpaRepository<AuthCredentials, Long> {
+    
+    // Custom query method required by AuthService to find credentials by the associated User entity
+    Optional<AuthCredentials> findByUser(User user);
 }
