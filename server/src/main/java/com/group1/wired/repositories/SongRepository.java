5c52420e288c87170d4a612e014f8df2907bd555
Martin <martin.mones@student.ateneo.edu>
2026-03-15 15:59:13 +0800

    added findByIds in album, artist, playlist, and song repositories

--- server/src/main/java/com/group1/wired/repositories/SongRepository.java
+++ server/src/main/java/com/group1/wired/repositories/SongRepository.java
@@ -4,7 +4,11 @@ import com.group1.wired.entities.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> {
+	
+    Optional<Song> findBySpotifyTrackId(String spotifyTrackId);
 
 }
