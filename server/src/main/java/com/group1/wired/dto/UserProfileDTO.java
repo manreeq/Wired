a479704346082afb3684869348d9e7d8dfb1ecca
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2026-04-20 10:02:34 +0800

    fixed UserProfileDTO not passing user id

--- server/src/main/java/com/group1/wired/dto/UserProfileDTO.java
+++ server/src/main/java/com/group1/wired/dto/UserProfileDTO.java
@@ -1,17 +1,24 @@
 package com.group1.wired.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserProfileDTO {
+    private Long userId;
     private String displayName;
     private String profilePicURL;
 
-    public UserProfileDTO(String displayName, String profilePicURL) {
+    public UserProfileDTO(Long userId, String displayName, String profilePicURL) {
+        this.userId = userId;
         this.displayName = displayName;
         this.profilePicURL = profilePicURL;
     }
 
-    public String getDisplayName() { return displayName; }
-    public String getProfilePicUrl() { return profilePicURL; }
-    
-    public void setDisplayName(String displayName) { this.displayName = displayName; }
-    public void setProfilePicUrl(String profilePicURL) { this.profilePicURL = profilePicURL; }
+//    public String getDisplayName() { return displayName; }
+//    public String getProfilePicUrl() { return profilePicURL; }
+//
+//    public void setDisplayName(String displayName) { this.displayName = displayName; }
+//    public void setProfilePicUrl(String profilePicURL) { this.profilePicURL = profilePicURL; }
 }
