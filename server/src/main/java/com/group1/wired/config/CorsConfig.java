7f6fe61bf5dfea9c7c3f3261b32e86a797a4804c
iel2408 <gphermosura2017@gmail.com>
2026-04-26 19:52:40 +0800

    fix 2

--- server/src/main/java/com/group1/wired/config/CorsConfig.java
+++ server/src/main/java/com/group1/wired/config/CorsConfig.java
@@ -5,6 +5,7 @@
 
 package com.group1.wired.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -16,13 +17,16 @@ import java.util.List;
 //mark this as a configuration class/file that launches on startup
 @Configuration
 public class CorsConfig {
+	
+	@Value("${allowed.origins}")
+    private String allowedOrigins;
+	
 	//registers this function as a spring boot managed component that runs on every request
     @Bean
-    
     public CorsFilter corsFilter() {
         CorsConfiguration configuration = new CorsConfiguration(); 								//new set of CORS rules
         configuration.setAllowCredentials(true);												//allows cookies to be sent with requests
-        configuration.setAllowedOrigins(List.of("${allowed.origins"));						//only accepts requests from local frontend
+        configuration.setAllowedOrigins(List.of(allowedOrigins));						//only accepts requests from local frontend
         configuration.setAllowedHeaders(List.of("*"));											//allows any headers in the request
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));	//allows only these http methods
 
